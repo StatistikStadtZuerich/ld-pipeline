d6e9ccd646733a170eb95a56836a1c994a9c02ce
Dominik Kuhn <dominik.kuhn@redlink.at>
2024-04-17 11:33:53 +0200

    first templating version

--- pipeline/base/services.py
+++ pipeline/base/services.py
@@ -1,8 +1,10 @@
-from .base import Base
+from ..interfaces.services import TemplateEngine, DbConnection
 from .environment import Config
+from typing import Dict
+from jinja2 import Environment, FileSystemLoader
 
 
-class DbConnection(Base):
+class DbConnection(DbConnection):
     """TODO defined the interface methods for DB connections"""
 
     def __init__(self, config: Config):
@@ -15,10 +17,35 @@ class DbConnection(Base):
         """
         return data
 
+    def open(self):
+        pass
 
-class TemplateEngine(Base):
+    def close(self):
+        pass
+
+
+class TemplateEngine(TemplateEngine):
     """TODO defined the interface methods for TemplateEngine"""
 
-    def __init__(self, config: Config, template_filepath: str, output_filepath: str):
+    def __init__(self, config: Config, template_filename: str, output_filename: str):
         super().__init__()
         self._config = config
+        self._env = Environment(loader=FileSystemLoader(config.get("template_path")))
+        self._template = self._env.get_template(template_filename)
+        self._output_path = config.get("output_path") + output_filename
+
+    def template(self, data: Dict):
+        content = self._template.render(data)
+        if not self._output_file.closed:
+            self._output_file.write(content)
+            print("wrote", self._output_path)
+        else:
+            print(
+                "File ist closed. Please open the file first and call the template function again."
+            )
+
+    def open(self):
+        self._output_file = open(file=self._output_path, mode="a", encoding="utf-8")
+
+    def close(self):
+        self._output_file.close()
---
+++ pipeline/interfaces/services.py
@@ -0,0 +1,41 @@
+from ..base.base import Base
+from ..base.environment import Config
+from typing import Dict
+from abc import abstractmethod
+
+
+class Closeable(Base):
+    @abstractmethod
+    def open(self):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
+
+
+class DbConnection(Closeable):
+    """TODO defined the interface methods for DB connections"""
+
+    @abstractmethod
+    def __init__(self, config: Config):
+        pass
+
+    @abstractmethod
+    def query(self, data):
+        """
+        TODO just a simple stub
+        """
+        pass
+
+
+class TemplateEngine(Closeable):
+    """TODO defined the interface methods for TemplateEngine"""
+
+    @abstractmethod
+    def __init__(self, config: Config, template_filename: str, output_filename: str):
+        pass
+
+    @abstractmethod
+    def template(self, data: Dict):
+        pass
