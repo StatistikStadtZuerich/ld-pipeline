398f08fc0afc61381807a67b862d9ab9ed0e51f7
Thomas Kurz <thomas.kurz@redbull.com>
2024-06-26 13:02:10 +0200

    HOTFIX reenable local testing

--- pipeline/base/__init__.py
+++ pipeline/base/__init__.py
@@ -1,16 +1,28 @@
 from .base import Base
 from .config import Env, Config
 from .environment import Environment
-from .services import MSSQLDbConnection, JinjaTemplateEngine, CompressionEngine
+from .services import JinjaTemplateEngine, CompressionEngine
 from .step import Step, StepDefinition
 from .utils import Utils
+import configparser
+import os
+
+config_path = os.path.join(os.path.dirname(__file__), '..', '..', 'config.ini')
+config = configparser.ConfigParser()
+config.read(os.path.join(config_path))
+
+db = config.get('DEFAULT', 'db')
+if db == 'mssql':
+    from .mmsql_environment import MMSqlEnvironment as Environment
+else:
+    from .environment import Environment
+
 
 __all__ = [
     "Base",
     "Env",
     "Config",
     "Environment",
-    "MSSQLDbConnection",
     "JinjaTemplateEngine",
     "CompressionEngine",
     "Step",
--- pipeline/base/services.py
+++ pipeline/base/services.py
@@ -2,7 +2,6 @@ import os
 import mysql.connector
 import gzip
 import re
-import pymssql
 
 from datetime import datetime
 from urllib.parse import quote
@@ -45,47 +44,6 @@ class MySQLDbConnection(DbConnection):
         self._connection.close()
         self.logger.info(
             f"Database connection to {self._config.get("mysql_host")}/{self._config.get("mysql_database")} closed"
-        )
-
-
-class MSSQLDbConnection(DbConnection):
-    def __init__(self, environment: "Environment"):
-        super().__init__()
-        self._config = environment.config
-
-    def query(self, query: str):
-        """
-        Executes query and returns cursor.
-        """
-        self._cursor.execute(query)
-        return self._cursor
-
-    def cursor(self):
-        return self._cursor
-    
-    def commit(self):
-        self._connection.commit()
-        
-    def rollback(self):
-        self._connection.rollback()
-
-    def __enter__(self):
-        self._connection = pymssql.connect(
-            server=self._config.get("mssql_host"),
-            database=self._config.get("mssql_database"),
-            user=self._config.get("mssql_user"),
-            password=self._config.get("mssql_password"),
-        )
-        self._cursor = self._connection.cursor(as_dict=True)
-        self.logger.info(
-            f"Database connection to {self._config.get('mssql_server')}/{self._config.get('mssql_database')} established..."
-        )
-        return self
-
-    def __exit__(self, *exc_details):
-        self._connection.close()
-        self.logger.info(
-            f"Database connection to {self._config.get('mssql_server')}/{self._config.get('mssql_database')} closed"
         )
 
 
