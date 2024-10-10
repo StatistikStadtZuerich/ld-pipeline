1323dcb3c4013b4b0909954cce2ffba3a1d1a616
sszwim <sszwim@host.docker.internal>
2024-10-10 18:27:49 +0200

    Only consider the highest hierarchy level per termset

--- sql/int/view_definition/view_vb_room_hierarchy_int.sql
+++ sql/int/view_definition/view_vb_room_hierarchy_int.sql
@@ -2,10 +2,33 @@ DROP VIEW IF EXISTS dbo.view_vb_room_hierarchy_int;
 GO
 
 CREATE VIEW dbo.view_vb_room_hierarchy_int AS
+WITH RankedTermsets AS (
+    SELECT
+        t.SASA_Job_Output_Id AS view_id,
+        value AS termset,
+        'Raum' AS dimension,
+        CASE
+            WHEN value = 'StadtZH' THEN 1
+            WHEN value = 'KreiseZH' THEN 2
+            WHEN value = 'QuartiereZH' THEN 3
+            ELSE 4
+        END AS termset_rank,
+        ROW_NUMBER() OVER (PARTITION BY t.SASA_Job_Output_Id ORDER BY 
+        CASE
+            WHEN value = 'StadtZH' THEN 1
+            WHEN value = 'KreiseZH' THEN 2
+            WHEN value = 'QuartiereZH' THEN 3
+            ELSE 4
+        END) AS rn
+    FROM
+        pipe_HDBDatenobjekte_TEST t
+    CROSS APPLY STRING_SPLIT(t.Raum_Hierarchie, ';')
+)
 SELECT
-    t.SASA_Job_Output_Id as view_id,
-    value as termset,
-	'Raum' as dimension
+    view_id,
+    termset,
+    dimension
 FROM
-    pipe_HDBDatenobjekte_TEST t
-CROSS APPLY STRING_SPLIT(t.Raum_Hierarchie, ';');
+    RankedTermsets
+WHERE
+    rn = 1;
