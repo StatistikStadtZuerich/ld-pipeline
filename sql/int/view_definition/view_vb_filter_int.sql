cd61f83f7f2b82bc1b57a28ff3ebb4d196fb68c9
sszwim <sszwim@host.docker.internal>
2024-11-18 21:35:27 +0100

    bugfix filter definitions

--- sql/int/view_definition/view_vb_filter_int.sql
+++ sql/int/view_definition/view_vb_filter_int.sql
@@ -3,12 +3,21 @@ GO
 
 CREATE VIEW dbo.view_vb_filter_int AS
 SELECT
-    t.SASA_Job_Output_Id as view_id,
-    value as termset,
-	CASE
-		WHEN value IN ('Jahr', 'Quartal') THEN 'ZEIT'
-		ELSE 'RAUM'
-	END as dimension
+	t.SASA_Job_Output_Id as view_id,
+	value as termset,
+	'Raum' AS dimension
 FROM
-    pipe_HDBDatenobjekte_TEST t
-CROSS APPLY STRING_SPLIT(t.Filter, ' ');
+	pipe_HDBDatenobjekte_TEST t
+CROSS APPLY STRING_SPLIT(t.Raum_Hierarchie, ';')
+WHERE
+	CHARINDEX(value, t.Filter) = 0
+UNION ALL
+SELECT
+	t.SASA_Job_Output_Id as view_id,
+	value as termset,
+	'Zeit' AS dimension
+FROM
+	pipe_HDBDatenobjekte_TEST t
+CROSS APPLY STRING_SPLIT(t.Zeit_Hierarchie, '|')
+WHERE
+	CHARINDEX(value, t.Filter) = 0;
