1be638c617eb33b34bb65a80436c22f923efd470
Lena Rubi <lena.rubi@zuerich.ch>
2025-08-21 15:51:13 +0200

    Remap Abgeleitete Gruppen und Time neu bauen
    - Code für das Zurückmappen der abgeleiteten Gruppen (AMU, HDM, etc.)
    - Bug fixes Verlinkung Prädikat/Objekt
    - Erstellen neuer Time-Variable via ZEIT Attribut

--- sql/int/view_definition/view_observation_int.sql
+++ sql/int/view_definition/view_observation_int.sql
@@ -5,36 +5,47 @@ GO
 CREATE VIEW dbo.view_observation_int
 AS
 SELECT
-	h.GESAMTCODE AS gesamtcode,
-	REPLACE(REPLACE(TRIM(h.CUBEID), 'CID_', ''), ' ', ',') as cube_ids,
-	h.KENNZAHL AS measure,
-	h.WERT AS value,
-	SUBSTRING(h.GESAMTCODE, 1, 9) AS time_code,
-	SUBSTRING(h.GESAMTCODE, 10, 6) AS room_code,
+    h.GESAMTCODE AS gesamtcode,
+    REPLACE(REPLACE(TRIM(h.CUBEID), 'CID_', ''), ' ', ',') AS cube_ids,
+    h.KENNZAHL AS measure,
+    h.WERT AS value,
+    SUBSTRING(h.GESAMTCODE, 1, 9)  AS time_code,
+	FORMAT(DATEFROMPARTS(CAST(SUBSTRING(h.ZEIT, 6, 4) AS INT), CAST(SUBSTRING(h.ZEIT, 4, 2) AS INT), CAST(SUBSTRING(h.ZEIT, 2, 2) AS INT)), 'yyyy-MM-dd') AS "time",
+    SUBSTRING(h.GESAMTCODE, 10, 6) AS room_code,
+
+    -- Property 1
     SUBSTRING(h.GESAMTCODE, 19, 3) AS prop1_code_short,
     CASE 
         WHEN g1.Origin IS NOT NULL 
         THEN g1.Origin + SUBSTRING(h.GESAMTCODE, 22, 4)
         ELSE SUBSTRING(h.GESAMTCODE, 19, 7)
     END AS prop1_code,
+
+    -- Property 2
     SUBSTRING(h.GESAMTCODE, 26, 3) AS prop2_code_short,
     CASE 
         WHEN g2.Origin IS NOT NULL 
         THEN g2.Origin + SUBSTRING(h.GESAMTCODE, 29, 4)
         ELSE SUBSTRING(h.GESAMTCODE, 26, 7)
     END AS prop2_code,
+
+    -- Property 3
     SUBSTRING(h.GESAMTCODE, 33, 3) AS prop3_code_short,
     CASE 
         WHEN g3.Origin IS NOT NULL 
         THEN g3.Origin + SUBSTRING(h.GESAMTCODE, 36, 4)
         ELSE SUBSTRING(h.GESAMTCODE, 33, 7)
     END AS prop3_code,
+
+    -- Property 4
     SUBSTRING(h.GESAMTCODE, 40, 3) AS prop4_code_short,
     CASE 
         WHEN g4.Origin IS NOT NULL 
         THEN g4.Origin + SUBSTRING(h.GESAMTCODE, 43, 4)
         ELSE SUBSTRING(h.GESAMTCODE, 40, 7)
     END AS prop4_code,
+
+    -- Property 5
     SUBSTRING(h.GESAMTCODE, 47, 3) AS prop5_code_short,
     CASE 
         WHEN g5.Origin IS NOT NULL 
@@ -42,17 +53,24 @@ SELECT
         ELSE SUBSTRING(h.GESAMTCODE, 47, 7)
     END AS prop5_code,
 
-	h.ANZ_GRUPPEN AS number_groups,
-	CASE
-		WHEN h.DATENSTATUS LIKE '%veröffentlicht%' THEN 'VEROEFFENTLICHT'
-		WHEN h.DATENSTATUS LIKE '%definitiv%' THEN 'DEFINITIV'
-		ELSE 'PROVISORISCH'
-	END as status
-	
-FROM
-	dbo.pipe_HDB_TEST h
+    h.ANZ_GRUPPEN AS number_groups,
+
+    CASE
+        WHEN h.DATENSTATUS LIKE '%veröffentlicht%' THEN 'VEROEFFENTLICHT'
+        WHEN h.DATENSTATUS LIKE '%definitiv%'     THEN 'DEFINITIV'
+        ELSE 'PROVISORISCH'
+    END AS status,
+	h.REFERENZNUMMER AS reference_number,
+	h.DATENSTAND as modified
+
+FROM dbo.pipe_HDB_TEST h
+	LEFT JOIN HDBAbgeleiteteGruppen g1 ON g1.Gruppe = SUBSTRING(h.GESAMTCODE, 19, 3)
+	LEFT JOIN HDBAbgeleiteteGruppen g2 ON g2.Gruppe = SUBSTRING(h.GESAMTCODE, 26, 3)
+	LEFT JOIN HDBAbgeleiteteGruppen g3 ON g3.Gruppe = SUBSTRING(h.GESAMTCODE, 33, 3)
+	LEFT JOIN HDBAbgeleiteteGruppen g4 ON g4.Gruppe = SUBSTRING(h.GESAMTCODE, 40, 3)
+	LEFT JOIN HDBAbgeleiteteGruppen g5 ON g5.Gruppe = SUBSTRING(h.GESAMTCODE, 47, 3)
 WHERE
-	h.RECORDSTATUS = '0'
+    h.RECORDSTATUS = '0'
 AND
 	h.CUBEID <> '';
 GO
