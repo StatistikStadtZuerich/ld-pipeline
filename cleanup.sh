a1e173e6f07ac063fecefd49326a30a602f59786
Jakob Frank <jakob.frank@redlink.at>
2025-12-04 10:12:08 +0100

    feat: verbesserte Konfigurierbarkeit von fuseki- und cleanup-script

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-DONE_DIR="/home/lod_pipeline/ld-pipeline-2024/output/int/done/"
-FUSEKI_INDEX_DIR="/home/lod_pipeline/ld-pipeline-2024/fuseki_index/int/"
-PIPELINE_DATA_DIR="/home/lod_pipeline/hdb_dropzone/prod/test/pipeline_data"
+ENV_NAME="${1:-local}"
+
+SCRIPT="$(readlink -f "$0")"
+SCRIPT_HOME="$(dirname "$SCRIPT")"
+
+function load_env() {
+  [ -r "$1" ] && . "$1" || true
+}
+
+# load local settings
+load_env "${SCRIPT_HOME}/.env"
+load_env "${SCRIPT_HOME}/${ENV_NAME}.env"
+load_env "./.env"
+load_env "./${ENV_NAME}.env"
+
+JENA_DIR="${JENA_DIR:-/home/lod_pipeline/apache-jena-fuseki-4.9.0/jena}"
+FUSEKI_INDEX_DIR="${FUSEKI_INDEX_DIR:-/home/lod_pipeline/ld-pipeline-2024/fuseki_index/${ENV_NAME}}"
+INPUT_DIR="${INPUT_DIR:-/home/lod_pipeline/ld-pipeline-2024/output/${ENV_NAME}}"
+DONE_DIR="$INPUT_DIR/done"
+PIPELINE_DATA_DIR="${PIPELINE_DATA_DIR:-/home/lod_pipeline/hdb_dropzone/prod/test/Pipeline_Data}"
 
 # 1) Lösche alle regulären Dateien im Ordner $DONE_DIR, die älter als 24 h sind
 find "$DONE_DIR" -type f -mmin +1440 -delete
