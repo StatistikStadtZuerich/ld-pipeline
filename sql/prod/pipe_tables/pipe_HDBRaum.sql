4fde84725fda85177361b823daeb78c4b48f6b3f
Lena Rubi <lena.rubi@icloud.com>
2026-03-04 16:04:53 +0100

    added scripts for pipe_HDBRaum generation
    - Aufnahme der historischen Raumdaten in die Tabelle
    - Neu gibt es die Spalten GueltigVon und GueltigBis; diese sind nur für Datenpunkte befüllt, welche jetzt schon einen Eintrag in GUELTIGKEITSBEREICHEID haben

---
+++ sql/prod/pipe_tables/pipe_HDBRaum.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS dbo.pipe_HDBRaum;
+
+SELECT
+    r.RAUM,
+    r.RAUMLANG,
+    r.RAUMPARENT,
+    r.RAUMPARENTLANG,
+    r.WIKIDATAURI,
+    r.BESCHREIBUNG,
+    r.GUELTIGKEITSBEREICHEID,
+    r.RAUMSORT,
+    r.RAUMHIERARCHIE,
+    CASE 
+        WHEN r.GUELTIGKEITSBEREICHEID IN ('alle', 'keine')
+             OR r.GUELTIGKEITSBEREICHEID IS NULL
+        THEN NULL
+        WHEN LEFT(r.GUELTIGKEITSBEREICHEID,4) LIKE '[0-9][0-9][0-9][0-9]'
+        THEN DATEFROMPARTS(CAST(LEFT(r.GUELTIGKEITSBEREICHEID,4) AS int), 1,1)
+        ELSE NULL
+    END AS GueltigVon,
+    CASE 
+        WHEN r.GUELTIGKEITSBEREICHEID IN ('alle', 'keine')
+             OR r.GUELTIGKEITSBEREICHEID IS NULL
+        THEN NULL
+        WHEN CHARINDEX('heute', r.GUELTIGKEITSBEREICHEID) > 0
+        THEN CAST('9999-12-31' AS date)
+        WHEN RIGHT(r.GUELTIGKEITSBEREICHEID,4) LIKE '[0-9][0-9][0-9][0-9]'
+        THEN DATEFROMPARTS(CAST(RIGHT(r.GUELTIGKEITSBEREICHEID,4) AS int), 12,31)
+        ELSE NULL
+    END AS GueltigBis
+
+INTO dbo.pipe_HDBRaum
+FROM dbo.HDBRaum r
+
+UNION ALL
+
+SELECT
+    h.LDID    AS RAUM,
+    h.Name    AS RAUMLANG,
+    h.RaumParent,
+    h.RaumParentLang,
+    h.WikidataURI,
+    h.Beschreibung,
+    h.GueltigkeitsbereicheID,
+    h.RaumSort,
+    h.RaumHierarchie,
+    h.GueltigVon,
+    h.GueltigBis
+FROM dbo.HDBRaumHistorisch h;
