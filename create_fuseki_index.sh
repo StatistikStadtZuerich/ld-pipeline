6ac7d5438d5a065f904530a991099ed56a14b269
Jakob Frank <jakob.frank@redlink.at>
2026-03-06 12:41:55 +0100

    chore: validate input files before creating Fuseki index

--- create_fuseki_index.sh
+++ create_fuseki_index.sh
@@ -67,6 +67,14 @@ for FILE in "$INPUT_DIR"/*.gz; do
     fi
 done
 
+# Validate all the input files
+RIOT_LOG="$TMP_DIR/riot.log"
+if ! "${JENA_DIR}/bin/riot" --validate "$TMP_DIR"/*.gz &>"$RIOT_LOG"; then
+  log "Invalid data-files, refuse to build fuseki-index"
+  cat "$RIOT_LOG"
+  exit 3
+fi
+
 # Combine all .gz files into a single .ttl file and then compress it into a single .gz file
 log "Combining all .gz files into a single gz file"
 gunzip -c "$TMP_DIR"/*.gz > "$TMP_DIR/${ENV_NAME}_combined_${CURRENT_DATETIME}.ttl" \
