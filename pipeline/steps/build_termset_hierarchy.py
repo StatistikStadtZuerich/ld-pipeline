a44657889eea72f3a90860b887c339cb8b0e238d
Thomas Kurz <thomas.kurz@redbull.com>
2026-03-11 08:17:23 +0100

    add testcase and fix issue with None values in hierarchy mapping

--- pipeline/steps/build_termset_hierarchy.py
+++ pipeline/steps/build_termset_hierarchy.py
@@ -12,10 +12,13 @@ class BuildTermsetHierarchy(Templating):
     def pre_process(self, row):
         rows = []
         for x in range(1, 4):
+            x_value = row.get(f"f{x}")
+            if not x_value:
+                break
             for y in range(0, x):
                 value = row.get(f"f{y}")
                 if not value:
-                    continue
+                    break
                 for relation in value.split(";"):
                     rows.append(
                         {
