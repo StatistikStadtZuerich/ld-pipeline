b6612cad8d803641aae4d2e7cd20d37ce27e4a6b
Thomas Kurz <thomas.kurz@redbull.com>
2026-03-25 18:02:57 +0100

    create BuildInfo step and add it to the pipeline

---
+++ pipeline/steps/buildInfo.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime, timezone
+
+from ..base import Step, Environment
+
+
+class BuildInfo(Step):
+
+    def __init__(self, options={}):
+        """
+        Adds build information to output
+        """
+        super().__init__()
+        self._options = options
+
+    def run(self, environment: Environment):
+        out_dir = environment.config.get("output_path")
+        os.makedirs(out_dir, exist_ok=True)
+        out_file = os.path.join(out_dir, 'info.n3')
+
+        now = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S")
+        with open(out_file, 'w') as f:
+            f.write(f'<https://ld.stadt-zuerich.ch/.well-known/void> <http://purl.org/dc/terms/created> "{now}"^^<http://www.w3.org/2001/XMLSchema#dateTime> .\n')
