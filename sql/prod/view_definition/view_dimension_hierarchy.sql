c4cf3c9874ee3ce825a5bcad0ded3ace9f52a86a
sszselma <selma.adali@zuerich.ch>
2026-04-28 14:27:27 +0200

    korrektur test sql pipe

--- sql/prod/view_definition/view_dimension_hierarchy.sql
+++ sql/prod/view_definition/view_dimension_hierarchy.sql
@@ -7,8 +7,8 @@ CREATE VIEW [dbo].[view_dimension_hierarchy] AS
         h.GRUPPENCODE as child_code,
         h.PARENTCODE as parent_code,
         TRIM(value) AS hierarchie_relation
-    FROM [dbo].[pipe_HDBGruppenliste] h
-    JOIN [dbo].[pipe_HDBGruppenliste] p
+    FROM [dbo].[pipe_HDBGruppenliste_prod] h
+    JOIN [dbo].[pipe_HDBGruppenliste_prod] p
         ON p.GRUPPENCODE = h.PARENTCODE
         CROSS APPLY STRING_SPLIT(p.HIERARCHIEIDLIST, ';')
 ;
