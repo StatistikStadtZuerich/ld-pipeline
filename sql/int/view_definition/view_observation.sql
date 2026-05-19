1c701798e3e0e78b3788be77463acbcb9f6e661e
sszselma <selma.adali@zuerich.ch>
2026-05-19 13:35:48 +0200

    URI anpassen und view_observation optimieren

--- sql/int/view_definition/view_observation.sql
+++ sql/int/view_definition/view_observation.sql
@@ -7,153 +7,48 @@ AS
 
 WITH base_data AS (
 SELECT
-    --h_filter.GESAMTCODE AS gesamtcode,
-    REPLACE(REPLACE(TRIM(h_filter.CUBEID), 'CID_', ''), ' ', ',') AS cube_ids,
-    h_filter.KENNZAHL AS measure,
-    h_filter.WERT AS value,
-    SUBSTRING(h_filter.GESAMTCODE, 1, 9)  AS time_code,
-    FORMAT(DATEFROMPARTS(z.JAHR, z.MONAT, z.TAG), 'yyyy-MM-dd') AS [time],
-    SUBSTRING(h_filter.GESAMTCODE, 10, 6) AS room_code,
-
-    -- Property 1
-    SUBSTRING(h_filter.GESAMTCODE, 19, 3) AS prop1_code_short,
-    CASE 
-        WHEN g1.Origin IS NOT NULL 
-        THEN g1.Origin + SUBSTRING(h_filter.GESAMTCODE, 22, 4)
-        ELSE SUBSTRING(h_filter.GESAMTCODE, 19, 7)
-    END AS prop1_code,
-
-    -- Property 2
-    SUBSTRING(h_filter.GESAMTCODE, 26, 3) AS prop2_code_short,
-    CASE 
-        WHEN g2.Origin IS NOT NULL 
-        THEN g2.Origin + SUBSTRING(h_filter.GESAMTCODE, 29, 4)
-        ELSE SUBSTRING(h_filter.GESAMTCODE, 26, 7)
-    END AS prop2_code,
-
-    -- Property 3
-    SUBSTRING(h_filter.GESAMTCODE, 33, 3) AS prop3_code_short,
-    CASE 
-        WHEN g3.Origin IS NOT NULL 
-        THEN g3.Origin + SUBSTRING(h_filter.GESAMTCODE, 36, 4)
-        ELSE SUBSTRING(h_filter.GESAMTCODE, 33, 7)
-    END AS prop3_code,
-
-    -- Property 4
-    SUBSTRING(h_filter.GESAMTCODE, 40, 3) AS prop4_code_short,
-    CASE 
-        WHEN g4.Origin IS NOT NULL 
-        THEN g4.Origin + SUBSTRING(h_filter.GESAMTCODE, 43, 4)
-        ELSE SUBSTRING(h_filter.GESAMTCODE, 40, 7)
-    END AS prop4_code,
-
-    -- Property 5
-    SUBSTRING(h_filter.GESAMTCODE, 47, 3) AS prop5_code_short,
-    CASE 
-        WHEN g5.Origin IS NOT NULL 
-        THEN g5.Origin + SUBSTRING(h_filter.GESAMTCODE, 50, 4)
-        ELSE SUBSTRING(h_filter.GESAMTCODE, 47, 7)
-    END AS prop5_code,
-
-    h_filter.ANZ_GRUPPEN AS number_groups,
-
-    CASE
-        WHEN h_filter.DATENSTATUS LIKE '%veröffentlicht%' THEN 'VEROEFFENTLICHT'
-        WHEN h_filter.DATENSTATUS LIKE '%definitiv%'     THEN 'DEFINITIV'
-        ELSE 'PROVISORISCH'
-    END AS status,
-    --h_filter.REFERENZNUMMER AS reference_number,
-    h_filter.DATENSTAND as modified
-
-FROM (SELECT * FROM [dbo].[pipe_HDB_int] WHERE Publikationsstatus <> 'veröffentlicht') AS h_filter
-    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g1 ON g1.Gruppe = SUBSTRING(h_filter.GESAMTCODE, 19, 3)
-    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g2 ON g2.Gruppe = SUBSTRING(h_filter.GESAMTCODE, 26, 3)
-    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g3 ON g3.Gruppe = SUBSTRING(h_filter.GESAMTCODE, 33, 3)
-    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g4 ON g4.Gruppe = SUBSTRING(h_filter.GESAMTCODE, 40, 3)
-    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g5 ON g5.Gruppe = SUBSTRING(h_filter.GESAMTCODE, 47, 3)
-    LEFT JOIN [dbo].[pipe_HDBZeit_int] z ON z.ZEIT = h_filter.ZEIT
-	LEFT JOIN [dbo].[pipe_Diffusionsereignisse_int] d ON h_filter.DiffusionsID = d.id
-WHERE
-	h_filter.RECORDSTATUS = '0'
-	AND
-	coalesce(d.StartDate, '') <= GETDATE() 
-	AND
-	h_filter.CUBEID <> ''
-UNION ALL
-SELECT
-    --h.GESAMTCODE AS gesamtcode,
+    CONCAT(h.KENNZAHL,'-',h.Gruppencode1,'-',h.Gruppencode2,'-',h.Gruppencode3,'-',h.Gruppencode4,'-',h.Gruppencode5,'-',h.RAUM,'-',h.Zeit) AS URI,
     REPLACE(REPLACE(TRIM(h.CUBEID), 'CID_', ''), ' ', ',') AS cube_ids,
     h.KENNZAHL AS measure,
     h.WERT AS value,
-    SUBSTRING(h.GESAMTCODE, 1, 9)  AS time_code,
+    h.ZEIT AS time_code,
     FORMAT(DATEFROMPARTS(z.JAHR, z.MONAT, z.TAG), 'yyyy-MM-dd') AS [time],
-    SUBSTRING(h.GESAMTCODE, 10, 6) AS room_code,
-
-    -- Property 1
-    SUBSTRING(h.GESAMTCODE, 19, 3) AS prop1_code_short,
-    CASE 
-        WHEN g1.Origin IS NOT NULL 
-        THEN g1.Origin + SUBSTRING(h.GESAMTCODE, 22, 4)
-        ELSE SUBSTRING(h.GESAMTCODE, 19, 7)
-    END AS prop1_code,
-
-    -- Property 2
-    SUBSTRING(h.GESAMTCODE, 26, 3) AS prop2_code_short,
-    CASE 
-        WHEN g2.Origin IS NOT NULL 
-        THEN g2.Origin + SUBSTRING(h.GESAMTCODE, 29, 4)
-        ELSE SUBSTRING(h.GESAMTCODE, 26, 7)
-    END AS prop2_code,
-
-    -- Property 3
-    SUBSTRING(h.GESAMTCODE, 33, 3) AS prop3_code_short,
-    CASE 
-        WHEN g3.Origin IS NOT NULL 
-        THEN g3.Origin + SUBSTRING(h.GESAMTCODE, 36, 4)
-        ELSE SUBSTRING(h.GESAMTCODE, 33, 7)
-    END AS prop3_code,
-
-    -- Property 4
-    SUBSTRING(h.GESAMTCODE, 40, 3) AS prop4_code_short,
-    CASE 
-        WHEN g4.Origin IS NOT NULL 
-        THEN g4.Origin + SUBSTRING(h.GESAMTCODE, 43, 4)
-        ELSE SUBSTRING(h.GESAMTCODE, 40, 7)
-    END AS prop4_code,
-
-    -- Property 5
-    SUBSTRING(h.GESAMTCODE, 47, 3) AS prop5_code_short,
-    CASE 
-        WHEN g5.Origin IS NOT NULL 
-        THEN g5.Origin + SUBSTRING(h.GESAMTCODE, 50, 4)
-        ELSE SUBSTRING(h.GESAMTCODE, 47, 7)
-    END AS prop5_code,
-
+    h.RAUM AS room_code,
+    h.Gruppe1 AS prop1_code_short,
+    h.Gruppe2 AS prop2_code_short,
+    h.Gruppe3 AS prop3_code_short,
+    h.Gruppe4 AS prop4_code_short,
+    h.Gruppe5 AS prop5_code_short,
+    COALESCE(g1.Origin + SUBSTRING(h.Gruppencode1, 4, 4), h.Gruppencode1) AS prop1_code,
+    COALESCE(g2.Origin + SUBSTRING(h.Gruppencode2, 4, 4), h.Gruppencode2) AS prop2_code,
+    COALESCE(g3.Origin + SUBSTRING(h.Gruppencode3, 4, 4), h.Gruppencode3) AS prop3_code,
+    COALESCE(g4.Origin + SUBSTRING(h.Gruppencode4, 4, 4), h.Gruppencode4) AS prop4_code,
+    COALESCE(g5.Origin + SUBSTRING(h.Gruppencode5, 4, 4), h.Gruppencode5) AS prop5_code,
     h.ANZ_GRUPPEN AS number_groups,
-
+    h.DATENSTAND AS modified,
     CASE
         WHEN h.DATENSTATUS LIKE '%veröffentlicht%' THEN 'VEROEFFENTLICHT'
-        WHEN h.DATENSTATUS LIKE '%definitiv%'     THEN 'DEFINITIV'
+        WHEN h.DATENSTATUS LIKE '%definitiv%'      THEN 'DEFINITIV'
         ELSE 'PROVISORISCH'
-    END AS status,
-    --h.REFERENZNUMMER AS reference_number,
-    h.DATENSTAND as modified
-
+    END AS status
 FROM [dbo].[pipe_HDB_int] h
-    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g1 ON g1.Gruppe = SUBSTRING(h.GESAMTCODE, 19, 3)
-    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g2 ON g2.Gruppe = SUBSTRING(h.GESAMTCODE, 26, 3)
-    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g3 ON g3.Gruppe = SUBSTRING(h.GESAMTCODE, 33, 3)
-    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g4 ON g4.Gruppe = SUBSTRING(h.GESAMTCODE, 40, 3)
-    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g5 ON g5.Gruppe = SUBSTRING(h.GESAMTCODE, 47, 3)
+    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g1 ON g1.Gruppe = h.Gruppe1
+    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g2 ON g2.Gruppe = h.Gruppe2
+    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g3 ON g3.Gruppe = h.Gruppe3
+    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g4 ON g4.Gruppe = h.Gruppe4
+    LEFT JOIN [dbo].[pipe_HDBAbgeleiteteGruppen_int] g5 ON g5.Gruppe = h.Gruppe5
     LEFT JOIN [dbo].[pipe_HDBZeit_int] z ON z.ZEIT = h.ZEIT
-WHERE
-    h.RECORDSTATUS = '0'
-	AND
-	h.PUBLIKATIONSSTATUS = 'veröffentlicht'
-	AND
-	h.CUBEID <> ''
-	)
-SELECT 
+    LEFT JOIN [dbo].[pipe_Diffusionsereignisse_int] d ON d.id = h.DiffusionsID
+    WHERE h.RECORDSTATUS = '0'
+      AND h.CUBEID <> ''
+      AND (
+            h.PUBLIKATIONSSTATUS = 'veröffentlicht'
+          OR
+            (h.Publikationsstatus <> 'veröffentlicht' AND COALESCE(d.StartDate, '') <= GETDATE())
+      )
+)
+SELECT
+    b.URI,
     b.cube_ids,
     b.measure,
     b.value,
