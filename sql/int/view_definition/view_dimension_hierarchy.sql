311ee9bd8f5940bc05e7b7cb5ac30486deaf018f
sszselma <selma.adali@zuerich.ch>
2026-06-05 08:13:54 +0200

    view leeren

--- sql/int/view_definition/view_dimension_hierarchy.sql
+++ sql/int/view_definition/view_dimension_hierarchy.sql
@@ -3,12 +3,9 @@ DROP VIEW IF EXISTS [dbo].[view_dimension_hierarchy_int];
 GO
 
 CREATE VIEW [dbo].[view_dimension_hierarchy_int] AS
-    SELECT 
-        h.GRUPPENCODE as child_code,
-        h.PARENTCODE as parent_code,
-        TRIM(value) AS hierarchie_relation
-    FROM [dbo].[pipe_HDBGruppenliste_int] h
-    JOIN [dbo].[pipe_HDBGruppenliste_int] p
-        ON p.GRUPPENCODE = h.PARENTCODE
-        CROSS APPLY STRING_SPLIT(p.HIERARCHIEIDLIST, ';')
+    SELECT
+        CAST(NULL AS NVARCHAR(255)) AS child_code,
+        CAST(NULL AS NVARCHAR(255)) AS parent_code,
+        CAST(NULL AS NVARCHAR(255)) AS hierarchie_relation
+    WHERE 1=0
 ;
