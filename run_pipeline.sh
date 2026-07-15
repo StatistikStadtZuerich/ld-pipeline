e0b51f42ab2af3887f4e0de0c0f98003c200184f
Jakob Frank <jakob.frank@redlink.at>
2026-07-15 12:42:51 +0200

    fix: ensure correct script directory context in `run_pipeline.sh`
    - Updated branch retrieval command to use correct directory context.
    - Added debug logging for Python script execution within the pipeline.

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -95,7 +95,7 @@ else
   GIT_REV="$(cd "$SCRIPT_HOME" && git rev-parse HEAD)"
 fi
 # Make sure we have the correct branch name
-branch=$(git rev-parse --abbrev-ref HEAD)
+branch=$(cd "$SCRIPT_HOME" && git rev-parse --abbrev-ref HEAD)
 # From now on, a read-lock is sufficient
 flock -sn 999 || { debug "Could not acquire read-lock"; exit 0; }
 
@@ -143,6 +143,7 @@ NOTIFY_ARGS=(
 "${SCRIPT_HOME:-.}/scripts/teams-notify.sh" pipeline-status --status started --icon "🚀" "${NOTIFY_ARGS[@]}"
 (
   cd "$SCRIPT_HOME" || { debug "Could not change to '$SCRIPT_HOME'"; exit 2; }
+  debug "${PY_VENV%/}/bin/python" "${SCRIPT_HOME}/run_pipeline.py" "${ARGS[@]}"
   "${PY_VENV%/}/bin/python" "${SCRIPT_HOME}/run_pipeline.py" "${ARGS[@]}"
 ) || {
   exit_code="$?"
