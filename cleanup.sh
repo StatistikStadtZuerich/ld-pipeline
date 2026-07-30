df16649a8ccd24acf5faea552afe2028e6d34812
Jakob Frank <jakob.frank@redlink.at>
2026-07-30 11:31:19 +0200

    refactor(#447): standardize environment variable handling and directory paths across scripts
    - Introduced `DROPZONE_BASE` / `DROPZONE_DIR` for standardized handling of the hdb_dropzone
    - Introduced `ENV_NAME_UC` for consistent uppercase environment naming.
    - Updated path computations to utilize `SCRIPT_HOME` for better flexibility.
    - Modified `LOGS_DIR` initialization and cleanup logic for improved clarity and maintainability.

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ENV_NAME="${1:-local}"
+ENV_NAME_UC="$(echo "$ENV_NAME" | tr '[:lower:]' '[:upper:]')"
 
 SCRIPT="$(readlink -f "$0")"
 SCRIPT_HOME="$(dirname "$SCRIPT")"
@@ -16,10 +17,13 @@ load_env "./.env"
 load_env "./${ENV_NAME}.env"
 
 JENA_DIR="${JENA_DIR:-/home/lod_pipeline/apache-jena-fuseki-4.9.0/jena}"
-FUSEKI_INDEX_DIR="${FUSEKI_INDEX_DIR:-/home/lod_pipeline/ld-pipeline-2024/fuseki_index/${ENV_NAME}}"
-INPUT_DIR="${INPUT_DIR:-/home/lod_pipeline/ld-pipeline-2024/output/${ENV_NAME}}"
+FUSEKI_INDEX_DIR="${FUSEKI_INDEX_DIR:-${SCRIPT_HOME%/}/fuseki_index/${ENV_NAME}}"
+INPUT_DIR="${INPUT_DIR:-${SCRIPT_HOME%/}/output/${ENV_NAME}}"
 DONE_DIR="$INPUT_DIR/done"
-PIPELINE_DATA_DIR="${PIPELINE_DATA_DIR:-/home/lod_pipeline/hdb_dropzone/prod/test/Pipeline_Data}"
+DROPZONE_BASE=${DROPZONE_BASE:-/home/lod_pipeline/hdb_dropzone}
+DROPZONE_DIR=${DROPZONE_DIR:-${DROPZONE_BASE%/}/${ENV_NAME_UC}}
+PIPELINE_DATA_DIR="${PIPELINE_DATA_DIR:-${DROPZONE_DIR%/}/Pipeline_Data}"
+LOGS_DIR="${PIPELINE_DATA_DIR:-${DROPZONE_DIR%/}/logs}"
 
 function cleanup_files() {
     local dir="$1"
