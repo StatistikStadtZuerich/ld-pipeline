7d2d5073a6cb839d681d7d55ecfde46041a23f29
Jakob Frank <jakob.frank@redlink.at>
2026-09-08 15:14:29 +0200

    fix: use portable `mktemp` pattern in `create_fuseki_indexes.sh` for temp dir creation

--- create_fuseki_indexes.sh
+++ create_fuseki_indexes.sh
@@ -100,7 +100,7 @@ function secure_copy() {
 }
 
 log "Start building Fuseki-Index for '$TARGET_ENV' with Run-ID '$RUN_ID' to '$FUSEKI_INDEX_DIR'"
-WORKING_DIR="$(mktemp -d -t "fuseki_$RUN_ID")"
+WORKING_DIR="$(mktemp -d "fuseki_$RUN_ID.XXXX")"
 trap 'rm -rf "$WORKING_DIR"' EXIT
 
 log "Moving Data-Input to the Working-Dir at $WORKING_DIR"
